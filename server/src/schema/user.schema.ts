570e774e0ac5205a9a1e557b896bb5033cf70f02
rajharsh81070 <rajharsh81070@gmail.com>
2024-02-14 01:32:35 +0530

    feat: added auth routes

---
+++ server/src/schema/user.schema.ts
@@ -0,0 +1,23 @@
+import { object, string, TypeOf } from 'zod'
+
+export const createUserSchema = object({
+  body: object({
+    name: string({ required_error: 'Name is required' }),
+    email: string({ required_error: 'Email is required' }).email(
+      'Invalid email'
+    ),
+    password: string({ required_error: 'Password is required' }),
+  }),
+})
+
+export const loginUserSchema = object({
+  body: object({
+    email: string({
+      required_error: 'Email is required',
+    }),
+    password: string({ required_error: 'Password is required' }),
+  }),
+})
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>['body']
+export type LoginUserInput = TypeOf<typeof loginUserSchema>['body']
